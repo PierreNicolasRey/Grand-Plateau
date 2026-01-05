eb6d16c78ef003c11f3a778d345f9f4f11052639
PierreNicolasRey <pnrey@hotmail.fr>
2026-01-05 16:04:35 +0100

    GP-B : Initialisation Projet Java/ Spring Boot

---
+++ Grand-Plateau-Back/src/main/java/fr/grand_plateau/GrandPlateauApplication.java
@@ -0,0 +1,13 @@
+package fr.grand_plateau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandPlateauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandPlateauApplication.class, args);
+	}
+
+}
---
+++ Grand-Plateau-Back/src/test/java/fr/grand_plateau/GrandPlateauApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.grand_plateau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandPlateauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
