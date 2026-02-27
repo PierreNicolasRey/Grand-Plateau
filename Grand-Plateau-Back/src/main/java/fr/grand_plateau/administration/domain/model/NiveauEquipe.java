d4f940ebc253fb7fa32ce16129af24bf845047a4
PierreNicolasRey <pnrey@hotmail.fr>
2026-02-27 10:40:39 +0100

    feat(admin): Add empty create method for NiveauEquipe and its UTs (RED)

--- Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/domain/model/NiveauEquipe.java
+++ Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/domain/model/NiveauEquipe.java
@@ -3,4 +3,15 @@ package fr.grand_plateau.administration.domain.model;
 import java.util.UUID;
 
 public record NiveauEquipe(UUID niveauEquipeId, String nom, String abreviation) {
+  public NiveauEquipe {
+
+  }
+  public static NiveauEquipe create(String nom, String abreviation) {
+    validateArguments(nom, abreviation);
+    return null;
+  }
+
+  private static void validateArguments(String nom, String abreviation) {
+
+  }
 }
---
+++ Grand-Plateau-Back/src/test/java/fr/grand_plateau/administration/domain/model/NiveauEquipeTest.java
@@ -0,0 +1,30 @@
+package fr.grand_plateau.administration.domain.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class NiveauEquipeTest {
+  @Test
+  void should_create_NiveauEquipe_successfully() {
+    // WHEN
+    NiveauEquipe result = NiveauEquipe.create("world tour", "wt");
+
+    // THEN
+    Assertions.assertAll(() -> {
+      assertNotNull(result);
+      assertEquals("World Tour", result.nom());
+      assertEquals("WT", result.abreviation());
+    });
+  }
+
+  @Test
+  void should_throw_IllegalArgumentException_when_nom_and_abreviation_malformed() {
+    Assertions.assertThrows(IllegalArgumentException.class, () -> NiveauEquipe.create("", "WT"));
+    Assertions.assertThrows(IllegalArgumentException.class, () -> NiveauEquipe.create(null, "WT"));
+    Assertions.assertThrows(IllegalArgumentException.class, () -> NiveauEquipe.create("World Tour", ""));
+    Assertions.assertThrows(IllegalArgumentException.class, () -> NiveauEquipe.create("World Tour", null));
+  }
+}
