3e85973799c70dc31f6e9066dce85f18c362bdf7
PierreNicolasRey <pnrey@hotmail.fr>
2026-02-28 15:43:56 +0100

    feat(admin): Add logic for PaysPersistenceAdapter (GREEN)

--- Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/infrastructure/out/persistence/adapter/PaysPersistenceAdapter.java
+++ Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/infrastructure/out/persistence/adapter/PaysPersistenceAdapter.java
@@ -2,6 +2,7 @@ package fr.grand_plateau.administration.infrastructure.out.persistence.adapter;
 
 import fr.grand_plateau.administration.application.ports.out.PaysRepository;
 import fr.grand_plateau.administration.domain.model.Pays;
+import fr.grand_plateau.administration.infrastructure.out.persistence.entity.PaysEntity;
 import fr.grand_plateau.administration.infrastructure.out.persistence.mapper.PaysPersistenceMapper;
 import fr.grand_plateau.administration.infrastructure.out.persistence.repository.SpringDataPaysRepository;
 import org.springframework.stereotype.Component;
@@ -22,24 +23,27 @@ public class PaysPersistenceAdapter implements PaysRepository {
   }
   @Override
   public Optional<Pays> findById(UUID id) {
-    return Optional.empty();
+    return this.jpaRepository.findById(id).map(this.paysPersistenceMapper::toDomain);
   }
 
   @Override
   public Optional<Pays> findByNom(String paysName) {
-    return Optional.empty();
+    return this.jpaRepository.findByNom(paysName).map(this.paysPersistenceMapper::toDomain);
   }
 
   @Override
   public List<Pays> findAll() {
-    return null;
+    return this.jpaRepository.findAll().stream().map(this.paysPersistenceMapper::toDomain).toList();
   }
 
   @Override
   public Pays save(Pays pays) {
-    return null;
+    PaysEntity paysSaved = this.jpaRepository.save(this.paysPersistenceMapper.toEntity(pays));
+    return this.paysPersistenceMapper.toDomain(paysSaved);
   }
 
   @Override
-  public void delete(UUID id) {}
+  public void delete(UUID id) {
+    this.jpaRepository.deleteById(id);
+  }
 }
