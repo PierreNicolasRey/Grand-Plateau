ed123b7b7b5ea11bd74d2898e5efa50ef807a1e0
PierreNicolasRey <pnrey@hotmail.fr>
2026-03-01 09:43:22 +0100

    feat(admin): Add logic for NiveauEquipeService (GREEN)

--- Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/domain/service/NiveauEquipeService.java
+++ Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/domain/service/NiveauEquipeService.java
@@ -2,6 +2,7 @@ package fr.grand_plateau.administration.domain.service;
 
 import fr.grand_plateau.administration.application.ports.in.NiveauEquipeInputPort;
 import fr.grand_plateau.administration.application.ports.out.NiveauEquipeRepository;
+import fr.grand_plateau.administration.domain.exception.NiveauEquipeNotFoundException;
 import fr.grand_plateau.administration.domain.model.NiveauEquipe;
 
 import java.util.List;
@@ -16,16 +17,18 @@ public class NiveauEquipeService implements NiveauEquipeInputPort {
 
   @Override
   public NiveauEquipe findById(UUID id) {
-    return null;
+    return this.niveauEquipeRepository.findById(id).orElseThrow(() -> new NiveauEquipeNotFoundException(id));
   }
 
   @Override
   public List<NiveauEquipe> findAll() {
-    return null;
+    return this.niveauEquipeRepository.findAll();
   }
 
   @Override
   public NiveauEquipe save(String nom, String abreviation) {
-    return null;
+    NiveauEquipe niveauEquipeToSave = NiveauEquipe.create(nom, abreviation);
+
+    return this.niveauEquipeRepository.save(niveauEquipeToSave);
   }
 }
