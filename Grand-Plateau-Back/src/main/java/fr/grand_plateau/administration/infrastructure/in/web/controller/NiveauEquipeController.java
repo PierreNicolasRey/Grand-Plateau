f83270759fdf19adc6e42636393f556df7c13726
PierreNicolasRey <pnrey@hotmail.fr>
2026-03-05 10:58:03 +0100

    feat(admin): Add logic for NiveauEquipeController (GREEN)

--- Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/infrastructure/in/web/controller/NiveauEquipeController.java
+++ Grand-Plateau-Back/src/main/java/fr/grand_plateau/administration/infrastructure/in/web/controller/NiveauEquipeController.java
@@ -1,10 +1,12 @@
 package fr.grand_plateau.administration.infrastructure.in.web.controller;
 
 import fr.grand_plateau.administration.application.ports.in.NiveauEquipeInputPort;
+import fr.grand_plateau.administration.domain.model.NiveauEquipe;
 import fr.grand_plateau.administration.infrastructure.in.web.dto.NiveauEquipeDTO;
 import fr.grand_plateau.administration.infrastructure.in.web.dto.NiveauEquipeRequest;
 import fr.grand_plateau.administration.infrastructure.in.web.mapper.NiveauEquipeWebMapper;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,16 +32,23 @@ public class NiveauEquipeController {
 
   @GetMapping("/{id}")
   public ResponseEntity<NiveauEquipeDTO> findById(@PathVariable("id") UUID niveauEquipeId) {
-    return null;
+    NiveauEquipe niveauEquipeDomain = this.niveauEquipeInputPort.findById(niveauEquipeId);
+
+    return new ResponseEntity<>(this.niveauEquipeWebMapper.toDTO(niveauEquipeDomain), HttpStatus.OK);
   }
 
   @GetMapping
   public ResponseEntity<List<NiveauEquipeDTO>> findAll() {
-    return null;
+    List<NiveauEquipeDTO> niveauEquipeDTOList =
+        this.niveauEquipeInputPort.findAll().stream().map(this.niveauEquipeWebMapper::toDTO).toList();
+
+    return new ResponseEntity<>(niveauEquipeDTOList, HttpStatus.OK);
   }
 
   @PostMapping
   public ResponseEntity<NiveauEquipeDTO> save(@RequestBody @Valid NiveauEquipeRequest request) {
-    return null;
+    NiveauEquipe niveauEquipeSaved = this.niveauEquipeInputPort.save(request.nom(), request.abreviation());
+
+    return new ResponseEntity<>(this.niveauEquipeWebMapper.toDTO(niveauEquipeSaved), HttpStatus.CREATED);
   }
 }
