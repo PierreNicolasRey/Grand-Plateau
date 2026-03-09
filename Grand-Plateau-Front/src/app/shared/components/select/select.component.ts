17fb7d34adec0f2af2367b68faf9f10d40e45f01
PierreNicolasRey <pnrey@hotmail.fr>
2026-03-09 10:25:32 +0100

    feat(shared): Add empty SelectComponent

---
+++ Grand-Plateau-Front/src/app/shared/components/select/select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gp-select-component',
+  imports: [],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+})
+export class SelectComponent {
+
+}
